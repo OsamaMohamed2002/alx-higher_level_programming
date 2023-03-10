fe81d96d82dd7d2b86f00a3f0d993e178e9f3342
OsamaMohamed2002 <omohame594@gmail.com>
2023-03-10 14:35:40 +0200

    fix errors in Task_3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    from sys import argv
-    arg_sum = 0
-    for i in range(1, len(argv)):
-        arg_sum += int(argv[i])
-    print("{:d}".format(arg_sum)
+    import sys
+
+    n = len(sys.argv) - 1
+    sum = 0
+    for i in range(n):
+        sum = sum + int(sys.argv[i + 1])
+    print("{}" .format(sum))
